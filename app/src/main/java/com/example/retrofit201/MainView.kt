7ec47d1fcb237d056de63473baf067ab825d6c3e
atkmehmet <durmazmehmet47@gmail.com>
2024-12-05 23:07:59 +0300

    service connect viewModel

--- app/src/main/java/com/example/retrofit201/MainView.kt
+++ app/src/main/java/com/example/retrofit201/MainView.kt
@@ -1,10 +1,36 @@
 package com.example.retrofit201
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class MainView(private val service: Service):ViewModel() {
+    private var _userList by mutableStateOf<List<User>>(emptyList())
+        private set
 
+    var  error by mutableStateOf("")
+    init {
+
+        fetchUser()
+    }
+
+    fun fetchUser(){
+        try {
+
+            viewModelScope.launch {
+
+                val users = service.getUsers()
+                _userList = users
+            }
+        }  catch (e:Exception){
+            error = e.message?:""
+        }
+
+    }
 }
 class MainViewModelfactory:ViewModelProvider.Factory{
     override fun <T : ViewModel> create(modelClass: Class<T>): T = MainView(MyApplication.service) as T
